925337949d88cc8c5abd8d7702a0b2fd19eba4e6
megan levy <69728544+megan-levy@users.noreply.github.com>
2021-05-26 14:26:28 -0400

    Single Day Forecast created

---
+++ src/weatherForecast1Day.js
@@ -0,0 +1,34 @@
+import react from "react";
+import weatherIcon from "./weatherIcon";
+
+export default function WeatherForecastDay(props) {
+    function day() {
+        let date = new Date(props.data.dt * 1000);
+        let day = date.getDay();
+    
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    
+        return days[day];
+      }
+    function tempHigh () {
+        let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+    }
+    function tempLow () {
+        let temperature = Math.round(props.data.temp.low);
+    return `${temperature}°`;
+    }
+
+    return (
+        <div>
+            <div className = "WeatherForecastDay">
+                {day()}
+            </div>
+            <WeatherIcon code={props.data.weather[0].icon} size={30} />
+            <div className = "ForecastHighLow">
+                <span className ="high">HIGH: {tempHigh()}</span>
+                <span className = "low">LOW: {tempLow()}</span>
+            </div>
+        </div>
+    );
+}
